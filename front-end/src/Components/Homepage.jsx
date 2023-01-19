c9465a0584097ac4dc785bf2bcdb2c9183cbf7cd
Poornima Murali <poornimamuralip@gmal.com>
2023-01-19 20:24:57 +0530

    Home page card design statrd

--- front-end/src/Components/Homepage.jsx
+++ front-end/src/Components/Homepage.jsx
@@ -3,6 +3,7 @@ import "./Homepage.css"
 
 function Homepage() {
     return (
+
         <div className='main' >
             <header>
                 <nav className="navbar">
@@ -22,9 +23,61 @@ function Homepage() {
                             <span>Art.</span>Design Blog
                         </h1>
                         <p className='paragraph-lorem' >The public is more familiar with bad design than good design. <br />  It is, in effect, conditioned to prefer bad design, because that is what it lives with. <br /> The new becomes threatening, the old reassuring.</p>
+                        <div id='btn-tag' >
+                            <input type="text" placeholder='Search' id='srchs' />
+                        </div>
+
                     </div>
                 </div>
             </header>
+            <section className='design' id='design' >
+                <div className="container-head">
+                    <div className="title">
+                        <h2>Arts</h2>
+                    </div>
+                </div>
+                <div className="design-cnt">
+                    <div className="design-item">
+                        <img src="https://wallpaperaccess.com/full/352112.jpg" alt="" />
+                    </div>
+                    <div className="design-title">
+                        <a href="">Make an awesome art</a>
+                    </div>
+                </div>
+
+            </section>
+            <section className='design' id='design' >
+                <div className="container-head">
+                    <div className="title">
+                        <h2>Arts</h2>
+                    </div>
+                </div>
+                <div className="design-cnt">
+                    <div className="design-item">
+                        <img src="https://wallpaperaccess.com/full/352112.jpg" alt="" />
+                    </div>
+                    <div className="design-title">
+                        <a href="">Make an awesome art</a>
+                    </div>
+                </div>
+
+            </section>
+            <section className='design' id='design' >
+                <div className="container-head">
+                    <div className="title">
+                        <h2>Arts</h2>
+                    </div>
+                </div>
+                <div className="design-cnt">
+                    <div className="design-item">
+                        <img src="https://wallpaperaccess.com/full/352112.jpg" alt="" />
+                    </div>
+                    <div className="design-title">
+                        <a href="">Make an awesome art</a>
+                    </div>
+                </div>
+
+            </section>
         </div>
     )
 }
