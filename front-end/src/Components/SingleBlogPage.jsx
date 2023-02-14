39bc699cbead6a0072256384e6e6947ef19c93e0
Poornima Murali <poornimamuralip@gmal.com>
2023-02-14 20:47:07 +0530

    View Cmnt section part completed

--- front-end/src/Components/SingleBlogPage.jsx
+++ front-end/src/Components/SingleBlogPage.jsx
@@ -26,6 +26,13 @@ function SingleBlogPage() {
                             <button className='cmnt-btn' >Submit</button>
                         </div>
                     </div>
+                    <div className="viewcmnt">
+                        <div className="cmnt_read">
+                            <h3 className='user-name'>Poornima</h3>
+                            <p className='cmnt-para' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quibusdam, ratione saepe commodi laudantium nam quae reiciendis impedit eius optio consequuntur aperiam cum excepturi fugit pariatur voluptates incidunt provident blanditiis?</p>
+                            <button className='view-cmnt' >View New Comments</button>
+                        </div>
+                    </div>
 
                 </div>
 
