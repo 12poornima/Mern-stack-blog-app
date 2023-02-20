6a02eb19612b1357183ac2711818114b5556b18b
Poornima Murali <poornimamuralip@gmal.com>
2023-02-20 13:16:46 +0530

    completed blog editing page

--- front-end/src/Components/BlogEditing.jsx
+++ front-end/src/Components/BlogEditing.jsx
@@ -6,25 +6,31 @@ function BlogEditing() {
         <div>
             <div className="center">
                 <div className="editing">
-                    <h1>Edit Your Blog</h1>
+                    <h1 className='head-title' >Edit Your Blog</h1>
                     <form id='frm1' >
                         <div className='txt' >
-                            <input type="text" required className='input' />
                             <label >Title</label>
+                            <input type="text" required className='input' />
                         </div>
-                        <div className="editing">
-                            <input type="text" required className='input2' />
+                        <div className="editing1">
                             <label htmlFor="">Discription</label>
+                            <input type="text" required className='input2' />
+                            {/* <textarea name="" id="" cols="30" rows="10"></textarea> */}
                         </div>
-                        <div className="editing">
-                            <input type="text" placeholder='Edit Your image' />
-                            {/* <label htmlFor="">Image</label> */}
+                        <div className="editing2">
+                            <label htmlFor="">Image</label>
+                            <input type="text" />
                         </div>
-                        <div className="editing">
+                        <div className="editing3">
                             <input type="date" required className='input3' />
+                            {/* <label htmlFor="">Date </label> */}
+
                         </div>
-                        <div className="editing">
-                            <select name="">
+                        <div className="custom-select">
+
+                            <select className='select-div' >
+                                <option value="0">Categary</option>
+
                                 <option value="Arts">Arts</option>
                                 <option value="Sports">Sports</option>
                                 <option value="Tech">Tech</option>
@@ -32,13 +38,13 @@ function BlogEditing() {
                                 <option value="Others">Others</option>
                             </select>
                         </div>
-                        <button>SUBMIT</button>
+                        <button className='btn-for-sub' >SUBMIT</button>
 
 
                     </form>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
